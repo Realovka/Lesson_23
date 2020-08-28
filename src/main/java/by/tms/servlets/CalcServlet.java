ae0839c151532a63d032d9e208f0aaccec65975b
realovka <realovka@mail.ru>
2020-08-28 18:44:05 +0300

    first commit

--- src/main/java/by/tms/servlets/CalcServlet.java
+++ src/main/java/by/tms/servlets/CalcServlet.java
@@ -1,5 +1,6 @@
 package by.tms.servlets;
 
+import by.tms.entity.Result;
 import by.tms.operation.*;
 
 import javax.servlet.ServletException;
@@ -7,13 +8,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@WebServlet(urlPatterns = "/", loadOnStartup = 1)
-public class HomeServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/calc", name = "CalcServlet")
+public class CalcServlet extends HttpServlet {
 
+    List<Result> results=new ArrayList<>();
 
     @Override
     public void init() throws ServletException {
@@ -21,26 +26,27 @@ public class HomeServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        HttpSession session=req.getSession();
         String a = req.getParameter("num1");
         String b = req.getParameter("num2");
         String type = req.getParameter("type");
-        getResponse(a, b, type, resp);
+        double result=getResponse(a, b, type, resp);
+        Result result1=new Result(a,b,type,result);
+        results.add(result1);
+        session.setAttribute("result", results);
+        resp.getWriter().println(result);
     }
 
-    private void getResponse(String a, String b, String type, HttpServletResponse resp){
+    private double getResponse(String a, String b, String type, HttpServletResponse resp) {
         Map<Type, Operation> hashMap = new HashMap<>();
-        double num1=Double.parseDouble(a);
-        double num2=Double.parseDouble(b);
+        double num1 = Double.parseDouble(a);
+        double num2 = Double.parseDouble(b);
         hashMap.put(Type.SUM, new Sum());
         hashMap.put(Type.SUBTR, new Subtr());
         hashMap.put(Type.DIV, new Div());
         hashMap.put(Type.MULT, new Mult());
-        try {
-            resp.getWriter().print(hashMap.get(Type.valueOf(type)).calc(num1,num2));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return hashMap.get(Type.valueOf(type)).calc(num1, num2);
     }
 
     @Override
